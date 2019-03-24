dcc23db7c627f41328850508a94018bd36a386c1
stochastiq <abigailjoylow@gmail.com>
2019-03-24 22:38:43 +1100

    change port to 8080

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT || 3000, process.env.IP, function(){
+app.listen(process.env.PORT || 8080, process.env.IP, function(){
     console.log("The YelpCamp Server has Started");
 })
